0548b4dc0ef6cc0b80b9acd2ee30b2201a9e4898
Ninaraquel <nynaferreira.souza@gmail.com>
2024-11-14 17:23:46 -0300

    Criando a classe principal e a interface repository

--- src/loja/model/Celular.java
+++ src/loja/model/Celular.java
@@ -2,16 +2,38 @@ package loja.model;
 
 public class Celular extends LojaEletronicos {
 
+	private String sistemaOperacional;
+	private int capacidadeMemoria;
+
 	public Celular(int codigo, String nomeProduto, String marca, double preco) {
 		super(codigo, nomeProduto, marca, preco);
 
 	}
 
+	public String getSistemaOperacional() {
+		return sistemaOperacional;
+	}
+
+	public void setSistemaOperacional(String sistemaOperacional) {
+		this.sistemaOperacional = sistemaOperacional;
+	}
+
+	public int getCapacidadeMemoria() {
+		return capacidadeMemoria;
+	}
+
+	public void setCapacidadeMemoria(int capacidadeMemoria) {
+		this.capacidadeMemoria = capacidadeMemoria;
+	}
+
 	public void exibirDetalhes() {
-		System.out.println("Detalhes do Produto:");
-		System.out.println("Nome: " + getNome());
-		System.out.println("Marca: " + getMarca());
-		System.out.println("Preço: R$ " + getPreco());
+
+		// Chama o método exibirDetalhes da superclasse
+		super.exibirDetalhes();
+
+		// Adiciona os detalhes específicos da subclasse
+		System.out.println("Sistema Operacional: " + getSistemaOperacional());
+		System.out.println("Capacidade de Memória: " + getCapacidadeMemoria() + "GB");
 
 	}
 
